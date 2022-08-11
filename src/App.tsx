4954eeb14561a7973ea2f906ad4b476b9635f068
Esa Nuurtamo <esa.nuurtamo@helsinki.fi>
2022-08-11 13:03:54 +0200

    saving state to localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,31 @@
 import { Whiteboard } from './components/Whiteboard'
 import { Sidebar } from './components/Sidebar'
 import { useRecoilCallback } from 'recoil'
-import { Element, elementListState, elementPositionStateFamily, elementStateSelectorFamily } from './state'
+import { Element, elementListState, elementStateSelectorFamily } from './state'
 import { useEffect } from 'react'
-
-let elementsDb: Element[] = [
-  {id: 'test1', position: [100, 300], color: '#fcba03'},
-  {id: 'test2', position: [75, 100], color: '#03fc14'},
-  {id: 'test3', position: [200, 220], color: '#fc03b5'}, 
-]
 
 const getElementsFromDb = async () => {
   //wait to simulate async fetching
-  await new Promise(resolve => setTimeout(resolve, 1000))
-  return elementsDb
+  return JSON.parse(localStorage.getItem('elements') || '[]')
 }
 
 const saveElementsToDb = async (savedElements: Element[]) => {
   //wait to simulate async fetching
   await new Promise(resolve => setTimeout(resolve, 1000))
-  elementsDb = savedElements
+  localStorage.setItem('elements', JSON.stringify(savedElements))
 }
 
 export const App = () => {
-  const saveElements = useRecoilCallback(({snapshot}) => async () => {
+  const saveElements = useRecoilCallback(({ snapshot }) => async () => {
     const elementIds = await snapshot.getPromise(elementListState)
     const elements = await Promise.all(elementIds.map(id => snapshot.getPromise(elementStateSelectorFamily(id))))
     saveElementsToDb(elements)
   }, [])
 
-  const fetchElements = useRecoilCallback(({snapshot, set}) => async () => {
+  const fetchElements = useRecoilCallback(({ set }) => async () => {
     const elements = await getElementsFromDb()
-    set(elementListState, elements.map(el=>el.id))
-    elements.forEach(el => set(elementStateSelectorFamily(el.id), el)) 
+    set(elementListState, elements.map((el: Element) => el.id))
+    elements.forEach((el: Element) => set(elementStateSelectorFamily(el.id), el))
     saveElementsToDb(elements)
   }, [])
 
@@ -43,17 +36,17 @@ export const App = () => {
 
   //save the state to database once in every 5 seconds
   useEffect(() => {
-    setTimeout(() => {
+    setInterval(() => {
       saveElements()
     }, 5000)
   }, [saveElements])
 
-  
+
 
   return (
     <>
-      <Whiteboard/>
-      <Sidebar/>
+      <Whiteboard />
+      <Sidebar />
     </>
   )
 }
